df4363c554c863c7b7f6da14fa5eaf248885fdee
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2016-01-03 07:36:01 +0900

    最新ニュースをtopic関係なしに提示するapiを追加

---
+++ api/v1/recently.php
@@ -0,0 +1,42 @@
+<?php
+    date_default_timezone_set('Asia/Tokyo');    # 応急処置
+    include "../../mysql/connect.php";
+    $db->select_db("news_datasets");
+
+    $DATE = date("Y-m-d", strtotime("-1 month"));
+
+    if($_GET['limit']) $LIMIT = $_GET['limit'];
+        else $LIMIT = 30;
+
+    if($_GET['page']) $PAGE = $_GET['page'];
+        else $PAGE = 1;
+
+    $START = ($PAGE - 1) * $LIMIT;
+
+    $result = $db->prepare("SELECT id, topic_id, name, source, url, host, body, updated_at FROM news WHERE ? < updated_at ORDER BY updated_at AND 20 < body_length DESC LIMIT ?, ?");
+    $result->bind_param('sii', $DATE, $START, $LIMIT);
+    $result->execute();
+
+    # -> output news
+    $result->bind_result($id, $topic_id, $name, $source, $url, $host, $body, $updated_at);
+
+    $topics = array('news' => array());
+    while($result->fetch()) {
+        $topics["news"][] = array(
+            'id' => $id,
+            'topic_id' => $topic_id,
+            'title' => $name,
+            'source' => $source,
+            'url' => $url,
+            'host' => $host,
+            'body' => $body,
+            'updated_at' => $updated_at
+        );
+    }
+
+    $result->close();
+
+    $json = json_encode($topics, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+    echo $json;
+?>
